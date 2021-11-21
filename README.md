5f35fc0dc623ed353cffc7ee6c44df230471520f
anujssmishra <anujssmishra@gmail.com>
2021-11-22 05:20:37 +0530

    Started with development of iOS and iPadOS.

---
+++ README.md
@@ -0,0 +1,7 @@
+# riderapp
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
---
+++ android/app/src/main/kotlin/com/hikearide/riderapp/rider_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hykearide.riderapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
